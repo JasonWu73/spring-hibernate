44713030dab60e2c681e5450a521614fae03cecb
吴仙杰 <wuxianjiezh@gmail.com>
2023-01-27 20:47:20 +0800

    Bean Scop

---
+++ src/main/java/org/example/springhibernate/BeanScopeDemoApp.java
@@ -0,0 +1,23 @@
+package org.example.springhibernate;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author 吴仙杰
+ **/
+public class BeanScopeDemoApp {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+            new ClassPathXmlApplicationContext("bean-scope-application-context.xml");
+
+        Coach coach1 = context.getBean("coach", Coach.class);
+        Coach coach2 = context.getBean("coach", Coach.class);
+
+        boolean isSame = coach1 == coach2;
+
+        System.out.println("coach1 == coach2: " + isSame);
+        System.out.println("coach1 memory location: " + coach1);
+        System.out.println("coach2 memory location: " + coach2);
+    }
+}
