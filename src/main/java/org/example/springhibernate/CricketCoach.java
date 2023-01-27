2a3ff5e814348a0d6d3ae9ff72c61b2052d51594
吴仙杰 <wuxianjiezh@gmail.com>
2023-01-27 13:47:19 +0800

    Setter Injection

---
+++ src/main/java/org/example/springhibernate/CricketCoach.java
@@ -0,0 +1,28 @@
+package org.example.springhibernate;
+
+/**
+ * @author 吴仙杰
+ **/
+public class CricketCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+        System.out.println("CricketCoach: inside setter method - setFortuneService");
+    }
+}
---
+++ src/main/java/org/example/springhibernate/SetterDemoApp.java
@@ -0,0 +1,20 @@
+package org.example.springhibernate;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author 吴仙杰
+ **/
+public class SetterDemoApp {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+
+        CricketCoach cricketCoach = context.getBean("cricketCoach", CricketCoach.class);
+
+        System.out.println(cricketCoach.getDailyWorkout());
+        System.out.println(cricketCoach.getDailyFortune());
+
+        context.close();
+    }
+}
