6b60341c05ad12fbc8a080e184e465cb9f022c93
吴仙杰 <wuxianjiezh@gmail.com>
2023-01-27 19:21:15 +0800

    Injecting literal value

--- src/main/java/org/example/springhibernate/CricketCoach.java
+++ src/main/java/org/example/springhibernate/CricketCoach.java
@@ -5,6 +5,9 @@ package org.example.springhibernate;
  **/
 public class CricketCoach implements Coach {
 
+    private String emailAddress;
+    private String team;
+
     private FortuneService fortuneService;
 
     public CricketCoach() {
@@ -24,5 +27,23 @@ public class CricketCoach implements Coach {
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
         System.out.println("CricketCoach: inside setter method - setFortuneService");
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 }
--- src/main/java/org/example/springhibernate/SetterDemoApp.java
+++ src/main/java/org/example/springhibernate/SetterDemoApp.java
@@ -15,6 +15,9 @@ public class SetterDemoApp {
         System.out.println(cricketCoach.getDailyWorkout());
         System.out.println(cricketCoach.getDailyFortune());
 
+        System.out.println(cricketCoach.getEmailAddress());
+        System.out.println(cricketCoach.getTeam());
+
         context.close();
     }
 }
