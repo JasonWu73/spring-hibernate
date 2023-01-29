456b36ff802b39988189dc11919ad2a70403ee8d
吴仙杰 <wuxianjiezh@gmail.com>
2023-01-29 11:40:03 +0800

    Dependency Injection: Field Injection

--- src/main/java/org/example/springhibernate/TennisCoach.java
+++ src/main/java/org/example/springhibernate/TennisCoach.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     // define a default constructor
@@ -26,11 +27,5 @@ public class TennisCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
-    }
-
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
     }
 }
