a253ddfcce894beb6f4fd51f0c1e740e7b3e9c0b
吴仙杰 <wuxianjiezh@gmail.com>
2023-01-29 11:25:44 +0800

    Dependency Injection: Method Injection

--- src/main/java/org/example/springhibernate/TennisCoach.java
+++ src/main/java/org/example/springhibernate/TennisCoach.java
@@ -28,10 +28,9 @@ public class TennisCoach implements Coach {
         return fortuneService.getFortune();
     }
 
-    // define a setter method
     @Autowired
-    public void setFortuneService(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside setFortuneService() method");
+    public void doSomeCrazyStuff(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
         this.fortuneService = fortuneService;
     }
 }
