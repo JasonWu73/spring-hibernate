ef54030f97b1bf8c2a33d51426f5c8b97423d669
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-01 16:16:28 +0800

    String Trimmer Editor

--- src/main/java/org/example/springhibernate/validator/CustomerController.java
+++ src/main/java/org/example/springhibernate/validator/CustomerController.java
@@ -1,17 +1,26 @@
 package org.example.springhibernate.validator;
 
+import cn.hutool.core.lang.Console;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    /**
+     * 自动处理表单参数: 去除 String 参数的前后空格, 且当仅包含空白字符时, 设置为 null.
+     */
+    @InitBinder
+    public void setStringTrimmer(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/show-form")
     public String showForm(Model model) {
@@ -26,6 +35,9 @@ public class CustomerController {
         Customer customer,
         BindingResult bindingResult
     ) {
+        // for debugging
+        Console.log("Last Name: |{}|", customer.getLastName());
+
         if (bindingResult.hasErrors()) {
             return "validator/customer-form";
         }
