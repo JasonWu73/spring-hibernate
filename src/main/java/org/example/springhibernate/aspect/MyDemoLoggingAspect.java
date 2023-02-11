d1675642c3cd942a4814f2e23eed3604b767ef09
吴仙杰 <wuxianjiezh@gmail.com>
2023-02-11 17:28:06 +0800

    @Before advice: pointcut declaration

--- src/main/java/org/example/springhibernate/aspect/MyDemoLoggingAspect.java
+++ src/main/java/org/example/springhibernate/aspect/MyDemoLoggingAspect.java
@@ -2,14 +2,19 @@ package org.example.springhibernate.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
+    @Pointcut("execution(* org.example.springhibernate.dao.*.*(..))")
+    private void pointcutForDaoPackageMethod() {
+    }
+
     // execution(modifiers-pattern? return-type-pattern declaring-type-pattern? method-name-pattern(param-pattern) throws-pattern?)
-    @Before("execution(* org.example.springhibernate.dao.*.*(..))")
+    @Before("pointcutForDaoPackageMethod()")
     public void beforeAddAccountAdvice() {
         System.out.println("=====> Executing @Before advice on the method");
     }
